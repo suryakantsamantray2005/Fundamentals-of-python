75de3a2426bae89fc71f8b751d42652732220267
suryakantsamantray2005 <suryakantsamantray2005@gmail.com>
2026-05-24 04:58:33 +0530

    Changes

---
+++ dsa.py/rough.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self,nums):
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i-1]==nums[i]:
+                return True
+        return False
+obj=Solution()
+print(obj.containsDuplicate([1,2,3,4]))
